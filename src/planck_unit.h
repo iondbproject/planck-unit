0d278635a2d7b1d8b3e95da2cc6cb77001312d2e
Kris Wallperington <Honibannchi@gmail.com>
2016-07-05 14:15:08 -0700

    Move serial_c_iface out of extern C

--- src/planck_unit.h
+++ src/planck_unit.h
@@ -28,6 +28,10 @@
 #ifndef PLANCKUNIT_H
 #define PLANCKUNIT_H
 
+#if defined(ARDUINO)
+#include "serial_c_iface.h"
+#endif
+
 #ifdef  __cplusplus
 extern "C" {
 #endif
@@ -40,7 +44,6 @@ extern "C" {
 /* If we are compiling for the arduino, include the serial interface
  * and overwrite filename constant to be blank to save memory. */
 #if defined(ARDUINO)
-#include "serial_c_iface.h"
 #if defined(__FILE__)
 #undef __FILE__
 #define __FILE__ ""

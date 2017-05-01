943875df551a4f2fc30b3679399efc2fbc462fb0
danaack <danak@shaw.ca>
2017-05-01 11:16:53 -0700

    Update planck-unit human output style
    Include "Total Time Spent". As time for each individual test was already included, this meant ouputting total time spent to execute all of the tests in the suite.

---
+++ cmake-build-debug/CMakeFiles/PlanckUnit.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/danaklamut/ClionProjects/planck-unit/examples/test_example.c" "/Users/danaklamut/ClionProjects/planck-unit/cmake-build-debug/CMakeFiles/PlanckUnit.dir/examples/test_example.c.o"
+  "/Users/danaklamut/ClionProjects/planck-unit/src/ion_time/ion_time.c" "/Users/danaklamut/ClionProjects/planck-unit/cmake-build-debug/CMakeFiles/PlanckUnit.dir/src/ion_time/ion_time.c.o"
+  "/Users/danaklamut/ClionProjects/planck-unit/src/planck_unit.c" "/Users/danaklamut/ClionProjects/planck-unit/cmake-build-debug/CMakeFiles/PlanckUnit.dir/src/planck_unit.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/PlanckUnit.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PlanckUnit.dir/examples/test_example.c.o"
+  "CMakeFiles/PlanckUnit.dir/src/planck_unit.c.o"
+  "CMakeFiles/PlanckUnit.dir/src/ion_time/ion_time.c.o"
+  "PlanckUnit.pdb"
+  "PlanckUnit"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PlanckUnit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
